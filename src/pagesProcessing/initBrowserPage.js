f0b9a81e19246b525407e60add6ca9ddd50c4ba3
fabien <fabien.huet@gmail.com>
2018-01-20 00:17:56 +0100

    Reduce window size

--- src/pagesProcessing/initBrowserPage.js
+++ src/pagesProcessing/initBrowserPage.js
@@ -23,7 +23,7 @@ async function initPage() {
   console.log('start init page');
 
   // Set size
-  await exportedPage.page.setViewport({ width: 1280, height: 800 });
+  await exportedPage.page.setViewport({ width: 1024, height: 300 });
 
   // Page crash => remove and recreate one
   await exportedPage.page.on('error', () => {
