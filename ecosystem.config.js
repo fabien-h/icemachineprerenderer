6fdf9c2fe094e844de7bc285f6c0fb2f3a73017c
fabien <fabien.huet@gmail.com>
2018-01-20 16:49:02 +0100

    reduce memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,44 +1,44 @@
 module.exports = {
-    /**
-     * Application configuration section
-     * http://pm2.keymetrics.io/docs/usage/application-declaration/
-     */
-    apps: [
-
-        // First application
-        {
-            name: 'icemachineprerenderer',
-            script: 'server.js',
-            env: {
-                COMMON_VARIABLE: 'true'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            },
-            args: ["--mongoURL", "mongodb://localhost:27017/local"],
-            interpreter: "node",
-            node_args: "",
-            instances: 1,
-            exec_mode: "fork",
-            watch: true,
-            ignore_watch: ["node_modules", ".git", "data"],
-            max_memory_restart: "500M",
-            merge_logs: true
-        },
-    ],
-
-    /**
-     * Deployment section
-     * http://pm2.keymetrics.io/docs/usage/deployment/
-     */
-    deploy: {
-        productionFabien: {
-            user: 'fabien',
-            host: '67.205.163.60',
-            ref: 'origin/master',
-            repo: 'https://github.com/fabien-h/icemachineprerenderer.git',
-            path: '/var/www/',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-        },
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: "icemachineprerenderer",
+      script: "server.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      },
+      args: ["--mongoURL", "mongodb://localhost:27017/local"],
+      interpreter: "node",
+      node_args: "",
+      instances: 1,
+      exec_mode: "fork",
+      watch: ["src", "server.js"],
+      ignore_watch: ["node_modules", ".git", "data"],
+      max_memory_restart: "250M",
+      merge_logs: true
     }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    productionFabien: {
+      user: "fabien",
+      host: "67.205.163.60",
+      ref: "origin/master",
+      repo: "https://github.com/fabien-h/icemachineprerenderer.git",
+      path: "/var/www/",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production"
+    }
+  }
 };
