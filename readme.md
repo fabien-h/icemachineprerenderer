3d41e459adb5785bdcf9e8d3cff2b34a3c3173df
fabien <fabien.huet@gmail.com>
2018-02-02 09:48:53 -0500

    upate readme and local port for mongo dev (+ prettier)

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 
 [Ice machine website](https://www.icemachinesplus.io/) will serve prerendered version of its pages. The service will serve two purpose :
 
-- It will take a list of URLs on `/postpagestoprerender`. Those URLs will be prerendered with [Chrome Headless](https://developers.google.com/web/updates/2017/04/headless-chrome) and the html content and the meta will be saved in a local MongoDB base.
-- It will serve the content cached on `/getprerenderdata?pagePath=__path__`. The main application will request it before serving it to the end client.
+* It will take a list of URLs on `/postpagestoprerender`. Those URLs will be prerendered with [Chrome Headless](https://developers.google.com/web/updates/2017/04/headless-chrome) and the html content and the meta will be saved in a local MongoDB base.
+* It will serve the content cached on `/getprerenderdata?pagePath=__path__`. The main application will request it before serving it to the end client.
 
 > Every 10 hours, the service will fetch the current sitemap and regenerate everything
 
@@ -11,17 +11,16 @@
 
 Make sure you have dependencies installed :
 
-- node (homebrew)
-- nodemon (npm)
-- pm2 (npm)
-- mongo (homebrew)
+* node (homebrew)
+* nodemon (npm)
+* pm2 (npm)
+* mongo (homebrew)
 
 `npm i` to get the local packages.
 
 To get dev started, in tow terminals :
 `npm run dev_server_localDB`
 `npm run dev_database`
-
 
 # Prod
 
@@ -31,7 +30,12 @@ Our production server is `67.205.163.60`. Before deployment, you need to install
 
 To monitor and debug the service, login to the server with `ssh yourUserName@67.205.163.60` ; then you can launch the monitor with `pm2 monit`.
 
-We need to have a cron that destroy all the chrome instances and restart the pm2 process every three hours.
+We need to have a cron that destroy all the chrome instances and restart the pm2 process every 30 minutes. SO, `crontab -e` and :
 
-    killall chrome
-    pm2 restart all
+    */30 * * * * killall chrome; pm2 kill ; pm2 flush; cd ; cd /var/www/current; pm2 start pm2.json --no-daemon
+
+* kill all the chrome instances
+* kill all the node processes
+* flush the logs
+* go to the current directory
+* start the node process
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /**
  * Modules
  */
-const http = require("http");
-const url = require("url");
-const mongodb = require("mongodb");
+const http = require('http');
+const url = require('url');
+const mongodb = require('mongodb');
 
 const getPrerenderData = require('./src/routes/getPrerenderData.js');
 const postPagesToPrerender = require('./src/routes/postPagesToPrerender.js');
@@ -12,24 +12,24 @@ const initBrowser = require('./src/pagesProcessing/initBrowser.js');
 /**
  * Connect to the mongo database
  */
- global.mongoBase;
- let mongoBaseUrl;
- if (process.argv.indexOf('--mongoURL') === -1)
- console.error('mongoURL is not set');
- else mongoBaseUrl = process.argv[process.argv.indexOf('--mongoURL') + 1];
- mongodb.MongoClient.connect(mongoBaseUrl, (err, client) => {
-   if (err) return console.error('Connection to database failed');
-   console.log('Service started');
-   global.mongoBase = client.db('local');
-  });
+global.mongoBase;
+let mongoBaseUrl;
+if (process.argv.indexOf('--mongoURL') === -1)
+  console.error('mongoURL is not set');
+else mongoBaseUrl = process.argv[process.argv.indexOf('--mongoURL') + 1];
+mongodb.MongoClient.connect(mongoBaseUrl, (err, client) => {
+  if (err) return console.error('Connection to database failed');
+  console.log('Service started');
+  global.mongoBase = client.db('local');
+});
 
 /**
  * Init pupeteer the browser pages
  */
- global.browser;
- global.browserPages = [];
- const PAGES_COUNT = 2;
- initBrowser(PAGES_COUNT);
+global.browser;
+global.browserPages = [];
+const PAGES_COUNT = 2;
+initBrowser(PAGES_COUNT);
 
 /**
  * The server will serve only two route :
@@ -40,18 +40,18 @@ const server = http.createServer((request, response) => {
   // Will answer to our two routes
   // otherwise, answer a 403
   switch (url.parse(request.url).pathname) {
-    case "/getprerenderdata":
+    case '/getprerenderdata':
       getPrerenderData(request, response, mongoBase);
       break;
-    case "/postpagestoprerender":
+    case '/postpagestoprerender':
       postPagesToPrerender(request, response, mongoBase);
       break;
     default:
       response.writeHead(403);
-      response.end("IceMachine prerenderer ; you have nothing to do there.");
+      response.end('IceMachine prerenderer ; you have nothing to do there.');
       break;
   }
 });
 
 server.listen(8080);
-console.log("***** APP STARTED *****");
+console.log('***** APP STARTED *****');
